5e12f2c9dfe40c47a7388c3e2341abecd8d70b0f
Seongmin Choi <sungpa1919@gmail.com>
2013-10-15 01:29:51 -0600

    Initialize repository

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,8 +6,52 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
-    @movies = Movie.all
+   def index
+    @all_ratings = Movie.all_ratings
+    redirect = false
+
+    logger.debug(session.inspect)
+
+    if params[:sort_by]
+      @sort_by = params[:sort_by]
+      session[:sort_by] = params[:sort_by]
+    elsif session[:sort_by]
+      @sort_by = session[:sort_by]
+      redirect = true
+    else
+      @sort_by = nil
+    end
+
+    if params[:commit] == "Refresh" and params[:ratings].nil?
+      @ratings = nil
+      session[:ratings] = nil
+    elsif params[:ratings]
+      @ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      redirect = true
+    else
+      @ratings = nil
+    end
+
+    if redirect
+      flash.keep
+      redirect_to movies_path :sort_by=>@sort_by, :ratings=>@ratings
+    end
+
+    if @ratings and @sort_by
+      @movies = Movie.where(:rating => @ratings.keys).find(:all, :order => (@sort_by))
+    elsif @ratings
+      @movies = Movie.where(:rating => @ratings.keys)
+    elsif @sort_by
+      @movies = Movie.find(:all, :order => (@sort_by))
+    else
+      @movies = Movie.all
+    end
+    if !@ratings
+      @ratings = Hash.new
+    end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+   def self.all_ratings
+      a = Array.new
+      self.select("rating").uniq.each {|e1| a.push(e1.rating)}
+      a.sort.uniq
+   end
 end
